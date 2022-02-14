756b266b5d6612835f54881e9e423a25aba1ca76
StianOek <66445281+StianOek@users.noreply.github.com>
2022-02-14 10:05:57 +0100

    made server serve react application

--- server/server.js
+++ server/server.js
@@ -1,12 +1,18 @@
 import express from 'express';
+import path from 'path'
 const app = express();
 
-const port = process.env.PORT || 3000;
 
-
-app.get("/", (req, res) => {
-    res.json({hello: "world"})
+app.use(express.static("../client/dist"))
+app.use((req,res,next) => {
+    res.sendFile(path.resolve("../client/dist/index.html"));
 })
+
+
+
+
+
+const port = process.env.PORT || 3000;
 
 const server = app.listen(port, () => {
     console.log("Server is running on http://localhost:" + server.address().port)
