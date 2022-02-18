b54c2ee1273ff83bf22aa78224c2b0c234b0e435
StianOek <66445281+StianOek@users.noreply.github.com>
2022-02-18 13:54:05 +0100

    test

--- client/__tests__/quiz.test.jsx
+++ client/__tests__/quiz.test.jsx
@@ -14,4 +14,18 @@ describe("QuizApp", () => {
     );
     expect(element.innerHTML).toMatchSnapshot();
   });
+
+  it("should render answers", () => {
+    const element = document.createElement("div");
+
+    ReactDOM.render(
+      <MemoryRouter>
+        <Answer correctAnswers={3} questionsAnswered={10} />
+      </MemoryRouter>
+    );
+    expect(element.querySelector("[data-testid=status]").textContent).toEqual(
+      "Your score is 3 / 10"
+    );
+    expect(element.innerHTML).toMatchSnapshot();
+  });
 });
